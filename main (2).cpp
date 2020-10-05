198d5cdf23cc4066c1cd4b177d65e0755e13624f
Misha Vozheiko <71126718+MishaVozheiko@users.noreply.github.com>
2020-10-05 10:20:34 +0300

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,14 @@
+#include "lab5.h"
+#include <iostream>
+
+int main() {
+	Car* Car1 = new Car("Audi A4 B7", 142, 60000.0);
+	Car Car2;
+	Car Car3(*Car1);
+
+	Car1->print();
+	Car2.print();
+	Car3.print();
+
+	return 0;
+}
